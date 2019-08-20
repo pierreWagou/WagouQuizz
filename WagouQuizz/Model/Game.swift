19d9463765157104c4e0dc323358d61e912aef1b
pierreWagou <pierre.romon@gmail.com>
2019-08-20 17:13:38 -0400

    yo le rap

---
+++ WagouQuizz/Model/Game.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+class Game {
+    var score = 0
+    
+    private var questions = [Question]()
+    private var currentIndex = 0
+    
+    var state: State = .ongoing
+    
+    enum State {
+        case ongoing, over
+    }
+    
+    var currentQuestion: Question {
+        return questions[currentIndex]
+    }
+    
+    func refresh() {
+        score = 0
+        currentIndex = 0
+        state = .over
+    }
+    
+    func answerCurrentQuestion(with answer: Bool) {
+        if (currentQuestion.isCorrect && answer) || (!currentQuestion.isCorrect && !answer) {
+            score += 1
+        }
+        goToNextQuestion()
+    }
+    
+    private func goToNextQuestion() {
+        if currentIndex < questions.count - 1 {
+            currentIndex += 1
+        } else {
+            finishGame()
+        }
+    }
+    
+    private func finishGame() {
+        state = .over
+    }
+}
